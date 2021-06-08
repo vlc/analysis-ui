639030a6ddc5aa612758c18ab738ad32eba2cfe8
Trevor Gerhardt <trevorgerhardt@gmail.com>
2021-06-08 09:32:53 +0800

    format

--- lib/components/map/destination-travel-time-distribution.tsx
+++ lib/components/map/destination-travel-time-distribution.tsx
@@ -113,8 +113,9 @@ export default memo(function DestinationTravelTimeDistribution() {
   const [lockedLatLng, setLockedLatLng] = useState<LatLng | false>(false)
   const [latlng, setLatLng] = useState<LatLng | null>(null)
   const [distribution, setDistribution] = useState<void | number[]>()
-  const [comparisonDistribution, setComparisonDistribution] =
-    useState<void | number[]>()
+  const [comparisonDistribution, setComparisonDistribution] = useState<
+    void | number[]
+  >()
   const surface: CL.AccessGrid = useSelector(selectSurface)
   const comparisonSurface: CL.AccessGrid = useSelector(selectComparisonSurface)
   const leaflet = useLeaflet()
